d6368bb7d88fd84731492cae53625fdd61bbf726
antcel07 <93091784+antcel07@users.noreply.github.com>
2022-03-10 18:29:54 +0300

    Initial commit

---
+++ TravelTripProject/Models/Siniflar/Anasayfa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Siniflar
+{
+    public class Anasayfa
+    {
+        [Key]
+        public int ID{get; set;}
+        public String Baslik { get; set; }
+        public String Aciklama { get; set; }
+    }
+}
---
+++ TravelTripProject/Models/Siniflar/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+//manage nuget packgesde entityframework paketini indirip sisteme ekleriz.
+
+
+namespace TravelTripProject.Models.Siniflar
+{
+    public class Context:DbContext
+        //miras kalıtım alıyoruz
+    {
+
+        public DbSet<Admin>Admins { get; set; }
+        public DbSet<AdresBlog> AdresBlogs { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Hakkimizda> Hakkimizdas { get; set; }
+        public DbSet<iletisim> iletisims { get; set; }
+        public DbSet<Yorumlar> Yorumlars { get; set; }
+       
+    }
+}
